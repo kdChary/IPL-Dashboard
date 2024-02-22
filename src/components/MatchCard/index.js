16a2e6acf9892d586ba2e5e0c196a4ec2f5acd11
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-22 13:40:20 +0000

    match-card added

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,28 @@
-// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {recentMatchDetails} = props
+
+  const {
+    competingTeamLogo,
+    competingTeam,
+    result,
+    matchStatus,
+  } = recentMatchDetails
+  const resultClass = matchStatus === 'Won' ? 'won' : 'lost'
+
+  return (
+    <li className="match-card-item">
+      <img
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+        className="competing-team-logo"
+      />
+      <h4 className="competing-team-name">{competingTeam}</h4>
+      <p className="match-result">{result}</p>
+      <p className={`match-result ${resultClass}`}>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
